b2cb03e30ed502f4e6a5d31047ef634275e7ae01
BSTluo <1946831552@qq.com>
2024-01-08 11:22:52 +0800

    modified:   package.json 	modified:   src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema } from 'koishi';
+import { } from 'koishi-plugin-word-core';
 
-export const name = 'word-core-grammar-basic'
+export const name = 'word-core-grammar-basic';
 
-export interface Config {}
+export interface Config { }
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({});
 
 export function apply(ctx: Context) {
   // write your plugin here
+  ctx.inject(['word'], ctx => {
+    if (!ctx.word) { return; }
+    ctx.word.statement.addStatement('添加', async (inData, ctx, session) => {
+      console.log(inData);
+      console.log(session.uid);
+    });
+  });
 }
