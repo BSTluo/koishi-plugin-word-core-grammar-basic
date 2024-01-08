e35ada257b890ac570f847b41bcd318ef9031d97
BSTluo <1946831552@qq.com>
2024-01-08 16:44:07 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -15,11 +15,12 @@ export function apply(ctx: Context) {
   // write your plugin here
   ctx.inject(['word'], ctx => {
     if (!ctx.word) { return; }
-    ctx.word.statement.addStatement('+', async (inData, session) => {
-      // 语法: (+:物品名称:数量:谁？)
-      // 语法: (+:物品名称:数量~数量:谁？)
-      // 语法: (+:物品名称:数量%:谁？)
 
+    // 增加物品
+    // 语法: (+:物品名称:数量:谁？)
+    // 语法: (+:物品名称:数量~数量:谁？)
+    // 语法: (+:物品名称:数量%:谁？)
+    ctx.word.statement.addStatement('+', async (inData, session) => {
       const saveCell = inData.wordData.saveDB;
       const uid = (inData.args.length >= 3) ? inData.args[2] : session.uid;
 
@@ -54,6 +55,57 @@ export function apply(ctx: Context) {
 
 
       const now: number = number + Number(addNum);
+      const ok = await ctx.word.user.updateItem(uid, saveCell, item, now);
+
+      if (ok)
+      {
+        return String(now);
+      } else
+      {
+        throw `物品 [${item}] 添加失败`;
+      }
+    });
+
+    
+    // 减少物品
+    // 语法: (-:物品名称:数量:谁？)
+    // 语法: (-:物品名称:数量~数量:谁？)
+    // 语法: (-:物品名称:数量%:谁？)
+    ctx.word.statement.addStatement('-', async (inData, session) => {
+      const saveCell = inData.wordData.saveDB;
+      const uid = (inData.args.length >= 3) ? inData.args[2] : session.uid;
+
+      const item = inData.args[0];
+      const number = await ctx.word.user.getItem(uid, saveCell, item);
+
+      const addNumTemp = inData.args[1];
+      let addNum = 0;
+      if (!/^\d+$/.test(addNumTemp) && !/^\d+\~\d+$/.test(addNumTemp) && !/^\d+%$/.test(addNumTemp)) { throw `物品 [${item}] 添加的数量 [${addNum}] 不为数字或标识`; }
+
+      switch (true)
+      {
+        case (/^\d+$/.test(addNumTemp)): {
+          addNum = Number(addNum);
+          break;
+        }
+
+        case (/^\d+\~\d+$/.test(addNumTemp)): {
+          const matchData = addNumTemp.match(/^(\d+)\~(\d+)$/);
+          addNum = randomNumber(Number(matchData[1]), Number(matchData[2]));
+          break;
+        }
+        case (/^\d+%$/.test(addNumTemp)): {
+          const matchData = addNumTemp.match(/^(\d+)%$/);
+          addNum = number * Number(matchData[1]);
+          break;
+        }
+
+        default: { break; }
+      }
+
+      // 我觉得需要个数据缓存，而不是刚刚完成就保存
+
+      const now: number = number - Number(addNum);
       const ok = await ctx.word.user.updateItem(uid, saveCell, item, now);
 
       if (ok)
